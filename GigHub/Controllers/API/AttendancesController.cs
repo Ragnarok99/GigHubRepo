eb34547450c0a6bfc3be9659cef54e82d16279f8
Emmanuel villegas <evc.naq@hotmail.com>
2017-04-16 21:43:17 -0500

    In process of implement notifications

---
+++ GigHub/Controllers/API/AttendancesController.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using System.Web.Http;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.API
+{
+
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+
+            var userId = User.Identity.GetUserId();
+            var exist = _dbContext.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId);
+            if (exist) return BadRequest("The attendace already exist");
+
+            var attendance = new Attendance()
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
